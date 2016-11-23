c955ff6aef02befbfc35811d3ce3c5b88ab8803b
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-22 21:21:20 -0600

    datetime imported

--- mongo.py
+++ mongo.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
+import datetime 
 client = MongoClient('mongodb://localhost:27017/')
 Obi = {
     "name" : "Obi-wan Kenobi", 
     "age" : 12 , 
     "homeworld" : "Tatooine"
-}
+    "birth_date" : "datetime.datetime"
+    }
 db = client.starwars
 db.Jedis.insert(Obi)
