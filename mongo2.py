4376b2449e4f688256d1688f0f172b04d8f781dc
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-24 01:54:12 -0600

    Querying data from python

--- mongo2.py
+++ mongo2.py
@@ -2,16 +2,21 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 print ("Hello World!")
 print("I love Python and MongoDB")
+amigos = list()
 try: 
     nombre  = str(raw_input('Permitame su nombre caballero:'))
     apellidos = str(raw_input('Permitame su apellidos'))
     profesion =  str(raw_input('Permitame su profesion caballero'))
+    for x in range(0,5):
+        amigo = str(raw_input('como se llama su nombre de su amigo'))
+        amigos.append(str(amigo))
 except ValueError: 
     print "No es un string"
 persona = {
     "nombre": nombre, 
     "apellidos": apellidos,
-    "profesion" :profesion 
+    "profesion" :profesion,
+    "amigos": amigos
 }
 db = client.starwars 
 db.Jedis.insert(persona)
