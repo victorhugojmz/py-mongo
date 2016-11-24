3caba1cf0b756306da2e7995ef584fa4fe96328d
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-24 02:31:06 -0600

    Printing Documents

--- mongo3.py
+++ mongo3.py
@@ -1,3 +1,8 @@
 from pymongo import MongoClient 
 client = MongoClient('mongodb://localhost:27017/')
 print("---------- Python and MongoDB Part 3")
+db = client.starwars 
+for documento in db.Jedis.find({}):
+    print documento 
+
+
