5ba0db8855177b6a6d1dc9207c0932ed7c346e7f
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-24 02:37:25 -0600

    Adding stuff

--- mongo4.py
+++ mongo4.py
@@ -1,3 +1,3 @@
-from pymongo import MongoClient 
+from pymongo import MongoClient, UpdateOne , DeleteOne
 client = MongoClient('mongodb://localhost:27017/')
 print("---------- Python and MongoDB Part 3")
