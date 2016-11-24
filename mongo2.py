23b5ca4388fec8e0ffb2eaefba9dba4d66372661
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-11-24 02:14:11 -0600

    third file created

--- mongo2.py
+++ mongo2.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient 
 client = MongoClient('mongodb://localhost:27017/')
-print ("Hello World!")
+import datetime
+print ("Hello Py World!")
 print("I love Python and MongoDB")
 amigos = list()
 try: 
@@ -11,7 +12,7 @@ try:
     num_int = str(raw_input('Permitame su numero interior'))
     colonia  = str(raw_input('Permitame su colonia'))
     ciudad =  str(raw_input('Permitame su ciudad caballero'))
-    for x in range(0,5):
+    for x in range(0,2):
         amigo = str(raw_input('como se llama su nombre de su amigo'))
         amigos.append(str(amigo))
 except ValueError: 
@@ -20,7 +21,8 @@ persona = {
     "nombre": nombre, 
     "apellidos": apellidos,
     "profesion" :profesion,
-    "amigos": amigos, 
+    "amigos": amigos,
+    "registro" :  datetime.datetime.utcnow(),
     "direccion" : { 
         "ciudad" : ciudad, 
         "calle" : calle, 
